6b9843b3ec66ff7d03b963113f212a0de081cf6a
superspindel <viktor.fallman@hotmail.com>
2016-11-07 19:40:38 +0100

    fixat shoppingcart för random items

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from src.database.Database import Database
+from src.common.item import item
 
 storeApp = Flask(__name__)
 storeApp.secret_key = "hfudsyf7h4373hfnds9y32nfw93hf"
@@ -7,7 +8,8 @@ storeApp.secret_key = "hfudsyf7h4373hfnds9y32nfw93hf"
 
 @storeApp.route('/')
 def storeHome():
-    return render_template('home.html')
+    storeList = randomItemForCart()
+    return render_template('home.html', shoppingcart=storeList)
 
 
 @storeApp.route('/Category/<string:cat_id>')
@@ -26,6 +28,13 @@ def search():
 @storeApp.before_first_request
 def initialize_database():
     Database.initialize()
+
+def randomItemForCart():
+    itemList = []
+    itemList.append(item("1","skinka", "39", "skinka med allt"))
+    itemList.append(item("2", "senap", "14", "senap till smörgås"))
+    itemList.append(item("3", "fisk", "99", "fisk från skärgården"))
+    return itemList
 
 if __name__ == '__main__':
     storeApp.run(port=4995)
