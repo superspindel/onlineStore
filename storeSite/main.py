7ba0674939c92612d85e0988b507166b92c3aa8b
superspindel <viktor.fallman@hotmail.com>
2016-12-09 14:32:27 +0100

    adminDictionary

--- storeSite/main.py
+++ storeSite/main.py
@@ -214,7 +214,7 @@ def removeCart(cart_id):
 def adminHome():
     if user.isAdmin(session):
         session['isAdmin'] = True
-        return render_template("adminBase.html")
+        return render_template("adminBase.html", dictionary=getAdminDict())
     return storeHome()
 
 @storeApp.route('/Admin/view/<string:activity>')
